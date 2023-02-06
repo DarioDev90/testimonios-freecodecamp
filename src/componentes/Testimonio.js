84b0d2b10d3f9825fec18928a5c4a80d13b4a4cc
EderHdz <dariotecsoft90@gmail.com>
2023-02-05 22:48:35 -0500

    second commit

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,7 +3,7 @@ import React from "react";
 function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio' src='../imagenes/testimonio-emma.png' alt=''/>
+      <img className='imagen-testimonio' src={require('../imagenes/testimonio-emma.png')} alt='Foto de Emma'/>
     </div>
   );
 }
